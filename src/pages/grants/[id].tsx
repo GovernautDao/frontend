a05c99aeeaedb88d994ba779015af9d7cf173fed
Orlando Ortega <orlandoortegajr@gmail.com>
2024-08-10 19:51:42 -0400

    create page progress

--- src/pages/grants/[id].tsx
+++ src/pages/grants/[id].tsx
@@ -4,7 +4,6 @@ import { grantsTestData } from "../../../data/grantsTestData";
 export default function ViewProposal() {
   const router = useRouter();
 
-  // TODO: get this from DB or potentially a context provider so we dont have to look up same info twice, we should already have all
   // if not most info from the index page
   const data = grantsTestData.find(
     (grant) => grant.id === parseInt(router.query.id as string)
--- src/pages/grants/create.tsx
+++ src/pages/grants/create.tsx
@@ -1,98 +1,123 @@
-import { IDKitWidget, ISuccessResult } from "@worldcoin/idkit";
-import { useAccount } from "wagmi";
-import { useState } from "react";
-import WorldcoinIcon from "../../icons/Worldcoin";
-import { ConnectKitButton } from "connectkit";
+import { useAccount } from 'wagmi';
+import { FormEvent, useState } from 'react';
+import { ConnectKitButton } from 'connectkit';
+
+interface GrantForm {
+  fundsToRaise: string;
+}
+
+const MAX_ETH = 10000;
 
 export default function CreateGrant() {
-  const { address, isDisconnected } = useAccount();
-  const [formData, setFormData] = useState({
+  const { isDisconnected } = useAccount();
+  // TODO: Figure out how to call the smart contract to check if the user has an approved proposal
+  const [isApproved, setIsApproved] = useState(true);
+  const [formData, setFormData] = useState<GrantForm>({
     //add the fields
-    title: "",
-    description: "",
+    fundsToRaise: '',
+  });
+  const [formErrors, setFormErrors] = useState<GrantForm>({
+    fundsToRaise: '',
   });
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     const { name, value } = e.target;
+
+    if (name === 'fundsToRaise') {
+      if (isNaN(Number(value))) {
+        setFormErrors({
+          ...formErrors,
+          fundsToRaise: 'Funds to raise must be a number',
+        });
+      } else if (Number(value) < 0) {
+        setFormErrors({
+          ...formErrors,
+          fundsToRaise: 'Funds to raise must be greater than 0',
+        });
+      } else if (Number(value) > MAX_ETH) {
+        setFormErrors({
+          ...formErrors,
+          fundsToRaise: `Funds to raise must be less than ${MAX_ETH}`,
+        });
+      } else {
+        setFormErrors({ ...formErrors, fundsToRaise: '' });
+      }
+    }
+
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = async (verified: ISuccessResult) => {
-    // Call smart contract here with verified data and formData
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    
+    console.log('submitting');
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 w-full">
-      <div className="bg-white p-8 rounded-3xl shadow-md w-full max-w-3xl">
-        <h2 className="text-2xl mb-4">Create Grant</h2>
-        <form>
-          <div className="mb-4">
-            <label htmlFor="title" className="block text-gray-700">
-              Title
-            </label>
-            <input
-              type="text"
-              id="title"
-              name="title"
-              value={formData.title}
-              onChange={handleChange}
-              className="w-full p-2 border border-gray-300 rounded-lg mt-1"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="description" className="block text-gray-700">
-              Description
-            </label>
-            <textarea
-              id="description"
-              name="description"
-              value={formData.description}
-              onChange={handleChange}
-              className="w-full p-2 border border-gray-300 rounded-lg mt-1"
-              rows={4}
-              required
-            />
-          </div>
-          {isDisconnected && (
-            <ConnectKitButton.Custom>
-              {({ show }) => {
-                return (
-                  <button
-                    onClick={show}
-                    className="flex justify-center items-center gap-x-4 transition-all no-underline border border-gray-900 h-[50px] px-6 rounded-xl text-black w-full mb-4"
-                  >
-                    Connect Wallet
-                  </button>
-                );
-              }}
-            </ConnectKitButton.Custom>
-          )}
-          <IDKitWidget
-            app_id={process.env.WLD_CLIENT_ID as `app_${string}`}
-            action="testing-verfication-action"
-            signal={address}
-            onSuccess={(verified) => handleSubmit(verified)} // use onSuccess to call your smart contract
-          >
-            {({ open }) => (
-              <button
-                className="flex justify-center items-center gap-x-4 transition-all no-underline bg-gray-900 border border-gray-900 h-[50px] px-6 rounded-xl text-white w-full"
-                onClick={open}
-              >
-                <WorldcoinIcon
-                  height={21}
-                  width={21}
-                  fill="white"
-                  stroke="white"
+    <>
+      {isDisconnected && (
+        <>
+          {/* TODO: Add a nicer modal and message to ensure the user has their wallet connected before they can get to this view */}
+          Wallet must be connected to create a grant
+          <ConnectKitButton.Custom>
+            {({ show }) => {
+              return (
+                <button
+                  onClick={show}
+                  className='flex justify-center items-center gap-x-4 transition-all no-underline border border-gray-900 h-[50px] px-6 rounded-xl text-black w-full mb-4'
+                >
+                  Connect Wallet
+                </button>
+              );
+            }}
+          </ConnectKitButton.Custom>
+        </>
+      )}
+      {!isDisconnected && !isApproved && (
+        // TODO: Add a nicer error page when they have no approved proposals
+        <div>
+          Unfortunately no access since user does not have an approved proposal
+        </div>
+      )}
+      {!isDisconnected && (
+        <div className='min-h-screen flex items-center justify-center bg-gray-100 w-full'>
+          <div className='bg-white p-8 rounded-3xl shadow-md w-full max-w-3xl'>
+            <h2 className='text-2xl mb-4'>Create Grant</h2>
+            <form onSubmit={(e) => handleSubmit(e)}>
+              <div className='mb-4'>
+                <label htmlFor='fundsToRaise' className='block text-gray-700'>
+                  Funds to Raise (ETH)
+                </label>
+                <input
+                  type='text'
+                  id='fundsToRaise'
+                  name='fundsToRaise'
+                  value={formData.fundsToRaise}
+                  onChange={handleChange}
+                  placeholder='Enter the amount of funds you want to raise (ex: 1000 means 1000 ETH)'
+                  //TODO: Make border for error field red in create grant page
+                  className='w-full p-2 border border-gray-300 rounded-lg mt-1'
+                  required
                 />
-                Verify and Submit
+                {formErrors.fundsToRaise.length > 0 && (
+                  <span className='text-red-500 text-sm'>
+                    {formErrors.fundsToRaise}
+                  </span>
+                )}
+              </div>
+              {/* TODO: Make the button fit the style of the page */}
+              <button
+                type='submit'
+                className='w-full bg-black text-white p-2 rounded-lg'
+              >
+                Create
               </button>
-            )}
-          </IDKitWidget>
-        </form>
-      </div>
-    </div>
+            </form>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
